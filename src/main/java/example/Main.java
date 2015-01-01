78d6ae9c27f4524a8b8f64d652e512c6dccbb680
Marco Yuen <marcoy@gmail.com>
2015-01-01 12:52:00 -0500

    Add logback-classic

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -2,9 +2,13 @@ package example;
 
 import example.configuration.ExampleConfiguration;
 import excimer.nrepl.NREPLServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
     public static void main(final String[] args) throws InterruptedException {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
         ctx.register(ExampleConfiguration.class);
@@ -12,9 +16,9 @@ public class Main {
 
         final NREPLServer nreplServer = ctx.getBean("nreplServer", NREPLServer.class);
         nreplServer.startServer();
-        System.out.println(nreplServer);
-        System.out.println(nreplServer.isRunning());
-        System.out.println(nreplServer.getPort());
+        LOGGER.info("nREPL server instance: {}", nreplServer);
+        LOGGER.info("nREPL isRunning: {}", nreplServer.isRunning());
+        LOGGER.info("nREPL port: {}", nreplServer.getPort());
 
         while (true) {
             Thread.sleep(10000);
